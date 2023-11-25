033cb7b7712ca7de9ff6ae1c02bc6e9ecca8e5f7
Ian Spresney <41807694+ItsSpres@users.noreply.github.com>
2023-11-25 09:57:21 -0800

    done

--- csd-310/module_7/movies_queries.py
+++ csd-310/module_7/movies_queries.py
@@ -1,11 +1,4 @@
-# Ian Spresney
-# CSD 310
-# 11/24/2023
-# Module 7.2 Assignment
-# Sources: https://www.geeksforgeeks.org/sql-using-python/
-
 import mysql.connector
-
 
 # Function to connect to the MySQL database
 def connect_to_database():
@@ -25,13 +18,13 @@ def connect_to_database():
         print("Error: {}".format(err))
         return None
 
-# Function to execute a query and print the results
-def execute_query(cursor, query, description):
-    print(description)
+# Function to execute a query and print the results with a custom name
+def execute_query(cursor, query, custom_name, format_string):
+    print(custom_name)
     cursor.execute(query)
     results = cursor.fetchall()
     for result in results:
-        print(result)
+        print(format_string.format(*result))
     print("\n")
 
 def main():
@@ -44,20 +37,28 @@ def main():
             cursor = db.cursor()
 
             # Query 1: Select all fields from the studio table
+            custom_name1 = "-- DISPLAYING Studio RECORDS --"
+            format_string1 = "Studio ID: {0}\nStudio Name: {1}\n"
             query1 = "SELECT * FROM studio;"
-            execute_query(cursor, query1, "-- DISPLAYING Studio RECORDS --")
+            execute_query(cursor, query1, custom_name1, format_string1)
 
             # Query 2: Select all fields from the genre table
+            custom_name2 = "-- DISPLAYING Genre RECORDS --"
+            format_string2 = "Genre ID: {0}\nGenre Name: {1}\n"
             query2 = "SELECT * FROM genre;"
-            execute_query(cursor, query2, "-- DISPLAYING Genre RECORDS --")
+            execute_query(cursor, query2, custom_name2, format_string2)
 
             # Query 3: Select movie names for movies with a run time of less than two hours
-            query3 = "SELECT film_name FROM film WHERE film_runtime < 120;"
-            execute_query(cursor, query3, "-- DISPLAYING Short Film RECORDS --")
+            custom_name3 = "-- DISPLAYING Short Film RECORDS --"
+            format_string3 = "Film Name: {0}\nRuntime: {1}\n"
+            query3 = "SELECT film_name, film_runtime FROM film WHERE film_runtime < 120;"
+            execute_query(cursor, query3, custom_name3, format_string3)
 
             # Query 4: Get a list of film names and directors ordered by director
-            query4 = "SELECT film_director FROM film ORDER BY film_director;"
-            execute_query(cursor, query4, "-- DISPLAYING Director RECORDS in Order --")
+            custom_name4 = "-- DISPLAYING Director RECORDS in Order --"
+            format_string4 = "Film Name: {0}\nDirector: {1}\n"
+            query4 = "SELECT film_name, film_director FROM film ORDER BY film_director;"
+            execute_query(cursor, query4, custom_name4, format_string4)
 
         except mysql.connector.Error as err:
             print("Error: {}".format(err))
